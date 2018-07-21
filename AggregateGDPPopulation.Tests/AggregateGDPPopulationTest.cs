e90aa9a476aa5a0c9fd6f91f72f1c1a5ba348211
Deepika Mohan <deepikamm12@gmail.com>
2018-07-21 12:08:21 +0530

    updated

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
-
+using System.IO;
+using AggregateGDPPopulation;
 namespace AggregateGDPPopulation.Tests
 {
     public class UnitTest1
@@ -8,7 +9,31 @@ namespace AggregateGDPPopulation.Tests
         [Fact]
         public void Test1()
         {
-
+            //string[] expected = File.ReadAllLines(@"../../../../AggregateGDPPopulation.Tests/expected-output.json");
+            Class1 c = new Class1();
+            c.GenerateOutput();
+            //string[] actual = File.ReadAllLines(@"../../../../AggregateGDPPopulation/output/output.json");
+            
+            //gdp.CalculateAggregateGdp();
+            StreamReader FileRead = new StreamReader("../../../../AggregateGDPPopulation/output/output.json");
+            StreamReader ExpectedFileRead = new StreamReader("../../../expected-output.json");
+            string actual = FileRead.ReadToEnd();
+            string expected = String.Empty;
+            while (true)
+            {
+                try
+                {
+                    string content = ExpectedFileRead.ReadLine().Trim();
+                    expected += content;
+                }
+                catch (Exception e)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(actual);
+            Console.WriteLine(expected);
+            Assert.Equal(expected, actual);
         }
     }
 }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,53 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace AggregateGDPPopulation
 {
     public class Class1
     {
+        class Object
+        {
+            public float GDP_2012 { get; set; }
+            public float Population_2012 { get; set; }
+        }
+        public void GenerateOutput()
+        {
+            string[] ReadFile = File.ReadAllLines(@"../../../../AggregateGDPPopulation/data/datafile.csv");
+            string[] headersIndex = ReadFile[0].Split(',');
+            int IndexOfCountry = Array.IndexOf(headersIndex, "\"Country Name\"");
+            int IndexOfPopulation = Array.IndexOf(headersIndex, "\"Population (Millions) 2012\"");
+            int IndexOfGdp = Array.IndexOf(headersIndex, "\"GDP Billions (USD) 2012\"");
+            StreamReader r = new StreamReader(@"../../../../AggregateGDPPopulation/data/country-continent-map.json");
+            var json = r.ReadToEnd();
+            Dictionary<string, string> items = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            Dictionary<string, Object> AggregatedData = new Dictionary<string, Object>();
+            for (int i = 1; i < ReadFile.Length; i++)
+            {
+                string[] SplitByCommaOfSingleLine = ReadFile[i].Split(',');
+                try
+                {
+                    string continent = items[SplitByCommaOfSingleLine[IndexOfCountry].Trim('"')];
+                    try
+                    {
+                        AggregatedData[continent].GDP_2012 += float.Parse(SplitByCommaOfSingleLine[IndexOfGdp].Trim('"'));
+                        AggregatedData[continent].Population_2012 += float.Parse(SplitByCommaOfSingleLine[IndexOfPopulation].Trim('"'));
+                    }
+                    catch
+                    {
+                        Object newObject = new Object()
+                        {
+                            GDP_2012 = float.Parse(SplitByCommaOfSingleLine[IndexOfGdp].Trim('"')),
+                            Population_2012 = float.Parse(SplitByCommaOfSingleLine[IndexOfPopulation].Trim('"'))
+                        };
+                        AggregatedData.Add(continent, newObject);
+                    }
+                }
+                catch { }
+            }
+            var output = Newtonsoft.Json.JsonConvert.SerializeObject(AggregatedData);
+            File.WriteAllText(@"../../../../AggregateGDPPopulation/output/output.json", output);
+        }
     }
 }
